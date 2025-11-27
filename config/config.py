cf38de4e3591c9c9aee8e12355692691aa9a7a2d
Arbitr-crypto <olsq4701@gmail.com>
2025-11-27 20:16:17 +0300

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+TELEGRAM_TOKEN = "ВАШ_ТОКЕН_ТУТ"
+OWNER_CHAT_ID = 123456789  # ваш TG ID
+SPREAD_THRESHOLD = 0.015   # 1.5% спред
+MIN_VOLUME_USD = 1500      # минимальный объём
+MAX_COINS = 150            # максимум монет для сканирования
+CHECK_INTERVAL = 60         # интервал проверки, сек
+
+EXCHANGES = ["kucoin", "bitrue", "bitmart", "gateio", "poloniex"]
+SUPPORT_NICK = "@Arbitr_IP"
